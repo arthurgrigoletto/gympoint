37ad36e060cac27ccbda01daee4dd3b743dcfbd7
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-12-29 22:15:31 -0300

    Added Registrations routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 
 const routes = new Router();
 
@@ -14,13 +15,25 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+/**
+ * Students
+ */
 routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 
+/**
+ * Plans
+ */
 routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.destroy);
+
+/**
+ * Registrations
+ */
+routes.get('/registrations', RegistrationController.index);
+routes.post('/registrations', RegistrationController.store);
 
 export default routes;
