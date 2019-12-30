504fa7860be45f37d487a35606026d13f02c1c0f
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-12-29 22:14:19 -0300

    Created Registration Migrate

---
+++ src/database/migrations/20191230003638-create-registrations.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('registrations', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'students', key: 'id' },
+        onUpgrade: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'plans', key: 'id' },
+        onUpgrade: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('registrations');
+  },
+};
